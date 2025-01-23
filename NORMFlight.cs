c7130746155a257ea40a293728bc7262f488fa58
Ei Ei Khin <s10269738@connect.np.edu.sg>
2025-01-23 14:35:54 +0800

    NORMFlight

---
+++ NORMFlight.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//==========================================================
+// Student Number	: S10269738E
+// Student Name	: EI EI KHIN
+// Partner Name	: THINN MYAT MYAT HTWE
+//==========================================================
+
+namespace S10269056_PRG2Assignment
+{
+    public class NORMFlight : Flight
+    {
+        public NORMFlight(string flightNumber, string airlineCode, string origin, string destination, DateTime expectedTime, string status = "Scheduled")
+            : base(flightNumber, airlineCode, origin, destination, expectedTime, status) { }
+
+        public override int CalculateFee()
+        {
+            int fee = 300; // Base fee for all flights
+            if (Destination == "Singapore (SIN)") fee += 500; // Arriving flight fee
+            if (Origin == "Singapore (SIN)") fee += 800; // Departing flight fee
+            return fee;
+        }
+    }
+}
